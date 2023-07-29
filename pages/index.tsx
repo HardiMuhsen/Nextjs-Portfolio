5868b82120151e54322d3f2893bc41919b2aef33
HardiMuhsen <hardymuh980@outlook.com>
2023-07-29 20:36:24 +0300

    Update with latest changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,32 @@ const Home: NextPage = () => {
       
     <div className=''>
       <Head>
-        <title>Hardi-Muhsin | Portfolio</title>
+        <title>Hardi Muhsen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="title" content="Hardi Muhsen" />
+        <meta name="description" content="Welcome To My Portfolio 🥰" />
+        <meta name="author" content="Hardi Muhsen" />
+        <meta name="keywords" content="portfolio, web design, graphic design, photography" />
+        <meta name="robots" content="index, follow" />
+
+
+        
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hardi-portfolio.netlify.app/" />
+        <meta property="og:title" content="Hardi Muhsen" />
+        <meta property="og:description" content="Welcome To My Portfolio 🥰" />
+        <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+       
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://hardi-portfolio.netlify.app/" />
+        <meta property="twitter:title" content="Hardi Muhsen" />
+        <meta property="twitter:description" content="Welcome To My Portfolio 🥰" />
+        <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
         <link rel="icon" href="assets/Hm.ico" />
+        <link rel="canonical" href="https://hardi-portfolio.netlify.app/" />
+
       </Head>
 
       <Main />
